ce3c25e7574b46bf88a330b7830a6dfc009bbd1a
Kyle Corry <kylecorry31@gmail.com>
2018-08-01 18:12:33 -0400

    Got image recognition working

--- src/ML4K.java
+++ src/ML4K.java
@@ -16,123 +16,287 @@ import com.google.appinventor.components.annotations.UsesLibraries;
 
 import android.app.Activity;
 
-import java.net.URLEncoder;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.HttpURLConnection;
+import com.google.gson.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.Base64;
 import java.util.Scanner;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 @DesignerComponent(version = YaVersion.LABEL_COMPONENT_VERSION,
-  description = "This provides an interface for the Machine Learning for Kids website.",
-  category = ComponentCategory.EXTENSION,
-  nonVisible = true,
-  iconName = "images/extension.png")
-@SimpleObject(external=true)
-@UsesPermissions(permissionNames = "android.permission.INTERNET") // might need library for json
+        description = "This provides an interface for the Machine Learning for Kids website.",
+        category = ComponentCategory.EXTENSION,
+        nonVisible = true,
+        iconName = "images/extension.png")
+@SimpleObject(external = true)
+@UsesPermissions(permissionNames = "android.permission.INTERNET")
 @UsesLibraries(libraries = "gson.jar")
 public final class ML4K extends AndroidNonvisibleComponent {
 
-  private final Activity activity;
+    private static final String ENDPOINT_URL = "https://machinelearningforkids.co.uk/api/scratch/%s/classify";
+    private static final String DATA_KEY = "data";
 
-  private String key = "";
-  private final String baseURL = "https://machinelearningforkids.co.uk/api/scratch/";
-  private final String endpointURL = "/classify?data=";
+    private final Activity activity;
 
-  public ML4K(ComponentContainer container) {
-    super(container.$form());
-    activity = container.$context();
-  }
+    private String key = "";
 
-  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING)
-  @SimpleProperty(description = "The API key for the ML4K app.")
-  public void Key(String key) {
-    this.key = key;
-  }
+    public ML4K(ComponentContainer container) {
+        super(container.$form());
+        activity = container.$context();
+    }
 
-  @SimpleProperty
-  public String Key() {
-    return key;
-  }
+    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING)
+    @SimpleProperty(description = "The API key for the ML4K app.")
+    public void Key(String key) {
+        this.key = key;
+    }
 
-  @SimpleFunction(description = "Get the classification for the text.")
-  public void ClassifyText(final String data) {
-    final String urlStr = generateURL(data);
-    AsynchUtil.runAsynchronously(new Runnable() {
-      @Override
-      public void run() {
+    @SimpleProperty
+    public String Key() {
+        return key;
+    }
 
-        try {
-
-          // Pull from URL
-          URL url = new URL(urlStr);
-          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-          conn.setReadTimeout(10000);
-          conn.setConnectTimeout(10000);
-          conn.setRequestMethod("GET");
-          conn.setRequestProperty("Content-Type", "application/json");
-          conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0");
-
-          Scanner scanner = new Scanner(conn.getInputStream());
-          String json = scanner.next();
-          conn.disconnect();
-
-          // Parsing
-          JsonElement jsonElement = new JsonParser().parse(json);
-          JsonArray jsonArray = jsonElement.getAsJsonArray();
-          JsonObject value = jsonArray.get(0).getAsJsonObject();
-
-          final String className = value.get("class_name").getAsString();
-          final double confidence = value.get("confidence").getAsDouble();
-
-
-
-          // Dispatch the event.
-          activity.runOnUiThread(new Runnable(){
+    @SimpleFunction(description = "Get the classification for the image.")
+    public void ClassifyImage(final String path) {
+        runInBackground(new Runnable() {
             @Override
             public void run() {
-              GotClassification(data, className, confidence);
+                try {
+                    // Get the data
+                    final String imageData = getImageData(path);
+                    String dataStr = "{\"data\": " + "\"" + URLEncoder.encode(imageData, "UTF-8") + "\"}";
+
+                    // Setup the request
+                    URL url = new URL(getURL());
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setFixedLengthStreamingMode(dataStr.length());
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Accept", "*/*");
+                    conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0");
+                    conn.setRequestProperty("Connection", "keep-alive");
+                    conn.setRequestProperty("Content-Type", "application/json");
+
+                    // Send image data
+                    conn.setDoOutput(true);
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(dataStr);
+                    os.flush();
+                    os.close();
+
+                    // Parse
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        final String json = read(conn.getInputStream());
+                        conn.disconnect();
+
+                        // Parse JSON
+                        try {
+                            Classification classification = Classification.fromJson(path, json);
+                            GotClassification(classification.data, classification.classification, classification.confidence);
+                        } catch (JsonParseException e) {
+                            GotError(path, "Bad data from server: " + json);
+                        }
+                    } else {
+                        GotError(path, "Bad response from server: " + conn.getResponseCode());
+                        conn.disconnect();
+                    }
+
+                } catch (UnsupportedEncodingException e) {
+                    GotError(path, "Could not encode image");
+                } catch (MalformedURLException e) {
+                    GotError(path, "Could not generate URL");
+                } catch (IOException e) {
+                    GotError(path, "No Internet connection.");
+                }
+
             }
-          });
-        } catch(Exception e){
-          e.printStackTrace();
-          // TODO: handle error properly
-          activity.runOnUiThread(new Runnable(){
+        });
+    }
+
+    @SimpleFunction(description = "Get the classification for the text.")
+    public void ClassifyText(final String data) {
+        runInBackground(new Runnable() {
             @Override
             public void run() {
-              GotClassification(data, "ERROR", 0);
+                try {
+                    // Get the data
+                    String urlStr = getURL() + "?data=" + URLEncoder.encode(data, "UTF-8");
+
+                    // Setup the request
+                    URL url = new URL(urlStr);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0");
+
+                    // Parse
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        final String json = read(conn.getInputStream());
+                        conn.disconnect();
+
+                        // Parse JSON
+                        try {
+                            Classification classification = Classification.fromJson(data, json);
+                            GotClassification(classification.data, classification.classification, classification.confidence);
+                        } catch (JsonParseException e) {
+                            GotError(data, "Bad data from server: " + json);
+                        }
+                    } else {
+                        GotError(data, "Bad response from server: " + conn.getResponseCode());
+                        conn.disconnect();
+                    }
+
+                } catch (UnsupportedEncodingException e) {
+                    GotError(data, "Could not encode text");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    GotError(data, "Could not generate URL");
+                } catch (IOException e) {
+                    GotError(data, "No Internet connection.");
+                }
+
+
             }
-          });
+        });
+    }
+
+    /**
+     * Event indicating that a classification got an error.
+     *
+     * @param data  The data
+     * @param error The error
+     */
+    @SimpleEvent
+    public void GotError(final String data, final String error) {
+        final Component component = this;
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                EventDispatcher.dispatchEvent(component, "GotError", data, error);
+            }
+        });
+    }
+
+    /**
+     * Event indicating that a classification has finished.
+     *
+     * @param data           The data
+     * @param classification The classification
+     * @param confidence     The confidence of the classification
+     */
+    @SimpleEvent
+    public void GotClassification(final String data, final String classification, final double confidence) {
+        final Component component = this;
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                EventDispatcher.dispatchEvent(component, "GotClassification", data, classification, confidence);
+            }
+        });
+    }
+
+    // Helpers
+
+    /**
+     * Read an input stream to a String.
+     *
+     * @param is The input stream.
+     * @return The data from the input stream as a String.
+     */
+    private String read(InputStream is) {
+        Scanner scanner = new Scanner(is);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (scanner.hasNextLine()) {
+            sb.append(scanner.nextLine());
         }
 
-      }
-    });
-  }
-
-  /**
-   * Event indicating that a classification has finished.
-   *
-   * @param data The data
-   * @param classification The classification
-   * @param confidence The confidence of the classification
-   */
-  @SimpleEvent
-  public void GotClassification(String data, String classification, double confidence) {
-    // invoke the application's "GotClassification" event handler.
-    EventDispatcher.dispatchEvent(this, "GotClassification", data, classification, confidence);
-  }
-
-  private String generateURL(String data){
-    try {
-      return baseURL + key + endpointURL + URLEncoder.encode(data, "UTF-8");
-    } catch (Exception e){
-      e.printStackTrace();
-      return "";
+        return sb.toString();
     }
-  }
+
+    /**
+     * Get the ENDPOINT_URL for ML4K.
+     *
+     * @return The ENDPOINT_URL with the key for ML4K.
+     */
+    private String getURL() {
+        return String.format(ENDPOINT_URL, key);
+    }
+
+    /**
+     * Turn the data of an image into base 64.
+     *
+     * @param path The path to the image.
+     * @return The data of the image as a base 64 string.
+     */
+    private String getImageData(final String path) {
+        try {
+            Scanner scanner = new Scanner(new FileReader(path));
+            StringBuilder sb = new StringBuilder();
+            while (scanner.hasNext()) {
+                sb.append(scanner.next());
+            }
+            scanner.close();
+            byte[] encodedBytes = Base64.getEncoder().encode(sb.toString().getBytes());
+            return new String(encodedBytes);
+        } catch (FileNotFoundException e) {
+            GotError(path, "File not found");
+        }
+        return "";
+    }
+
+
+    private void runInBackground(Runnable runnable) {
+        AsynchUtil.runAsynchronously(runnable);
+    }
+
+
+    private static class Classification {
+        private String data;
+        private String classification;
+        private double confidence;
+
+        private Classification(String data, String classification, double confidence) {
+            this.data = data;
+            this.classification = classification;
+            this.confidence = confidence;
+        }
+
+        private static Classification fromJson(String data, String json) throws JsonParseException {
+            JsonElement jsonElement = new JsonParser().parse(json);
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            JsonObject value = jsonArray.get(0).getAsJsonObject();
+
+            final String className = value.get("class_name").getAsString();
+            final double confidence = value.get("confidence").getAsDouble();
+            return new Classification(data, className, confidence);
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public String getClassification() {
+            return classification;
+        }
+
+        public void setClassification(String classification) {
+            this.classification = classification;
+        }
+
+        public double getConfidence() {
+            return confidence;
+        }
+
+        public void setConfidence(double confidence) {
+            this.confidence = confidence;
+        }
+    }
+
 
 }
